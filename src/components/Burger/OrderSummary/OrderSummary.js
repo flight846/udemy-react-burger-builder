c5825938c554974804a07d8a3d23cde78eb823d4
DESKTOP-MFQCCVU\yazid <yazid@decision-science.com>
2019-03-25 13:43:09 +0800

    Add lifecycle method toimprove performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,19 +5,18 @@ import Button from '../../UI/Button/Button';
 const OrderSummary = (props) => {
     const ingredientsSummary = Object.keys(props.ingredients)
         .map(igKey => {
-            return <li key={ igKey }><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: { props.ingredients[igKey] }</li>
+            return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
         })
-
     return (
         <Auxi>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
-                { ingredientsSummary }
+                {ingredientsSummary}
             </ul>
-            <p><strong>Total Price: ${ props.price }</strong></p>
-            <Button btnType="Danger" clicked={ props.purchaseCancelled }>CANCEL</Button>
-            <Button btnType="Success" clicked={ props.purchaseContinued }>CHECKOUT</Button>
+            <p><strong>Total Price: ${props.price}</strong></p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CHECKOUT</Button>
         </Auxi>
     )
 }
